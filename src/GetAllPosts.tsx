c4ba24811d4692acc36cb5cce76c034aa77b3e24
RasWChr <r.west.chr@gmail.com>
2026-08-17 10:24:23 +0200

    Get all posts working

--- src/GetAllPosts.tsx
+++ src/GetAllPosts.tsx
@@ -1,9 +1,54 @@
+import {useEffect, useState} from "react";
 
-export function App() {
-  return (
-    <div>
+export function GetAllPosts() {
 
-    </div>
-  );
+    const [posts, setPosts] = useState<Root[]>([])
+
+    useEffect(() => {
+        fetch('https://dummyjson.com/posts')
+            .then(res => res.json())
+            .then((json) => {
+                setPosts(json.posts)
+            });
+    }, []);
+
+    return <div>
+        {
+            posts.map(p => {
+                return <MyChildComponent key={p.id} post={p}/>
+            })
+        }
+    </div>;
 }
 
+interface MyChildComponentProps {
+    post: Post,
+
+}
+
+function MyChildComponent({post, removeProduct}: MyChildComponentProps) {
+
+    return <div>{post?.title} </div>
+}
+
+export interface Root {
+    posts: Post[]
+    total: number
+    skip: number
+    limit: number
+}
+
+export interface Post {
+    id: number
+    title: string
+    body: string
+    tags: string[]
+    reactions: Reactions
+    views: number
+    userId: number
+}
+
+export interface Reactions {
+    likes: number
+    dislikes: number
+}
